4721f5ea3a9c0d7751998e2a91d370d47329063a
Leandri Viviers <leandri.viviers@entelect.co.za>
2022-03-25 12:47:45 +0200

    Care4You completed

---
+++ src/components/Care4You/Care4YouAdmin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="card-text">
+      The Admin Journey describes the process of reviewing ThankYOU cards
+    </p>
+    <ol>
+      <li>Users</li>
+      <li>Journey</li>
+      <ul>
+        <li>Sentiment Analysis</li>
+        <li>Manual review</li>
+      </ul>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Care4YouAdmin",
+};
+</script>
+
+<style></style>
---
+++ src/components/Care4You/Care4YouReceiver.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p class="card-text">
+      The Receiver Journey describes the process of viewing ThankYOU cards
+    </p>
+    <ol>
+      <li>
+        Users
+        <ul>
+          <li>Employees</li>
+          <li>Executives</li>
+        </ul>
+      </li>
+      <li>Journey</li>
+      <li>Dashboards</li>
+      <ul>
+        <li>Hospital board</li>
+        <li>Department board</li>
+        <li>Personal board</li>
+      </ul>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Care4YouSender",
+};
+</script>
+
+<style></style>
